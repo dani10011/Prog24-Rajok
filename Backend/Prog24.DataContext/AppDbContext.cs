6de3e678f02933d2a659718ab14ef00efdd5b42a
dani10011 <okros.dani28@gmail.com>
2025-10-14 16:31:35 +0200

    Refactored backend Added GetFaculties

---
+++ Backend/Prog24.DataContext/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Prog24.DataContext.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog24.DataContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Define your DbSets here
+        public DbSet<Faculty> Faculty { get; set; }
+        // public DbSet<YourEntity> YourEntities { get; set; }
+    }
+}
---
+++ Backend/Prog24.WebAPI/Controllers/FacultyController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prog24.DataContext.Entities;
+using Prog24.Services.Services.Interfaces;
+
+namespace Prog24.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FacultyController : Controller
+    {
+        private readonly IFacultyService _facultyService;
+
+        public FacultyController(IFacultyService facultyService)
+        {
+            _facultyService = facultyService;
+        }
+
+        [HttpGet]
+        public async Task<List<Faculty>> GetFaculties()
+        {
+             var faculties = await _facultyService.GetFaculties();
+             return faculties;
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
